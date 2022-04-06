028205179ee8e8ecbab61c743bf339f5702e587c
Elcins <elcins.gasanovs@gmail.com>
2022-04-06 15:01:47 +0300

    mod 7 task 2-3

---
+++ project/src/mocks/reviews.ts
@@ -0,0 +1,43 @@
+import { Reviews } from '../types/reviews';
+
+export const reviews: Reviews = [
+  {
+    comment:
+      'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
+    date: 'Mon Feb 21 2022 21:43:58 GMT+0300 (Москва, стандартное время)',
+    id: 1,
+    rating: 4,
+    user: {
+      avatarUrl: 'img/avatar-angelina.jpg',
+      id: 3,
+      isPro: true,
+      name: 'Angelina',
+    },
+  },
+  {
+    comment:
+      'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
+    date: 'Mon Feb 21 2022 21:43:58 GMT+0300 (Москва, стандартное время)',
+    id: 2,
+    rating: 3,
+    user: {
+      avatarUrl: 'img/avatar-angelina.jpg',
+      id: 2,
+      isPro: true,
+      name: 'Fred',
+    },
+  },
+  {
+    comment:
+      'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
+    date: 'Mon Feb 21 2022 21:43:58 GMT+0300 (Москва, стандартное время)',
+    id: 3,
+    rating: 5,
+    user: {
+      avatarUrl: 'img/avatar-angelina.jpg',
+      id: 1,
+      isPro: false,
+      name: 'Veronica',
+    },
+  },
+];
